dd0217d21b87e3cd4354e242dcae07de53677b60
VictorNievas17 <victor.nievas@ansotec.com>
2025-05-15 20:58:24 +0200

    Requirements

--- backend/app.py
+++ backend/app.py
@@ -47,14 +47,6 @@ app.register_blueprint(google, url_prefix='/api/google')
 
 from pymongo.errors import PyMongoError
 
-@app.before_first_request
-def test_connection():
-    try:
-        mongo.cx.admin.command('ping')  # mongo.cx es el MongoClient interno de flask_pymongo
-        print("Ping a MongoDB Atlas exitoso!")
-    except PyMongoError as e:
-        print("Error al conectar a MongoDB Atlas:", e)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
